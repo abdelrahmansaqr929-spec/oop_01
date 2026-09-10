11bf86ae71e8cd6aae8fb1143691e431e5f14a78
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-09-10 09:38:00 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_01
+{
+    internal class DeliveryAddress
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace oop_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region answer1
+            //1-it is struct so it will be a valu type so When a DeliveryAddress variable is copied to another variable and the copy is modified,
+            //the original variable remains completely unchanged.
+            //2- it is class so it will be refranse type so When a Customer variable is copied into another variable,
+            //modifying the object through one variable will alter the object for both variables
+            #endregion
+
+
+        }
+    }
+}
